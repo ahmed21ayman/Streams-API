cd5287c8c92857d3fafca8fcbb5a5baebfb26493
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-27 03:39:03 +0300

    v2: try stream

--- src/App.java
+++ src/App.java
@@ -8,5 +8,20 @@ public class App {
         names.add("mohamed");
         names.add("ali");
         names.add("mansour");
+        System.out.println("maping");
+        names.stream().map(name -> name.toUpperCase());
+        names.stream().map(name -> name.toLowerCase());
+        System.out.println("------------------------");
+        System.out.println("filter");
+        names.stream().filter(name -> name.startsWith("a"));
+        System.out.println("------------------------");
+        System.out.println("sorted");
+        names.stream().sorted();
+        System.out.println("------------------------");
+        System.out.println("for each");
+        names.stream().forEach(name -> System.out.println(name));
+        System.out.println("------------------------");
+        System.out.println("Combining Operations");
+        names.stream().filter(name -> name.startsWith("m")).map(name -> name.toUpperCase()).sorted().forEach(name -> System.out.println(name));
     }
 }
